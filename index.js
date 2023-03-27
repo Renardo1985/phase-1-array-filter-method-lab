ed6205eb7b54c2079c2099c24b8408d4ea4728fc
Renardo Williams <rennwill@gmail.com>
2023-03-27 15:41:19 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string)
+{
+    return array.filter(item => item.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(array, string)
+{
+   return array.filter((item) => item.toLowerCase().startsWith(string.toLowerCase()))    
+}
+
+function matchName(array, string)
+{
+   return array.filter(item => item.name.toLowerCase() === string.toLowerCase())    
+}
